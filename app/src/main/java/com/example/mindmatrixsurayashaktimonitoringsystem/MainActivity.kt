ad147005dc23917c7b0f8075b24397f90a754660
Student account <student@gmail.com>
2026-05-21 16:46:43 +0530

    commit changes

---
+++ app/src/main/java/com/example/mindmatrixsurayashaktimonitoringsystem/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.mindmatrixsurayashaktimonitoringsystem
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var etGeneration: EditText
+    private lateinit var etConsumption: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        etGeneration = findViewById(R.id.etGeneration)
+        etConsumption = findViewById(R.id.etConsumption)
+        btnCalculate = findViewById(R.id.btnCalculate)
+        tvResult = findViewById(R.id.tvResult)
+
+        btnCalculate.setOnClickListener {
+
+            calculateEnergy()
+        }
+    }
+
+    private fun calculateEnergy() {
+
+        val generationText =
+            etGeneration.text.toString()
+
+        val consumptionText =
+            etConsumption.text.toString()
+
+        if (generationText.isEmpty() ||
+            consumptionText.isEmpty()) {
+
+            Toast.makeText(
+                this,
+                "Please enter all values",
+                Toast.LENGTH_SHORT
+            ).show()
+
+            return
+        }
+
+        val generation =
+            generationText.toDouble()
+
+        val consumption =
+            consumptionText.toDouble()
+
+        val savings =
+            generation * 8
+
+        val greenScore =
+            ((generation / consumption) * 100).toInt()
+
+        var status = ""
+
+        if (generation > consumption) {
+
+            status =
+                "Over Generation: Exporting Electricity to Grid"
+
+        } else if (generation == consumption) {
+
+            status =
+                "Balanced Energy Usage"
+
+        } else {
+
+            status =
+                "Using Grid Electricity"
+        }
+
+        tvResult.text =
+            "Solar Generation : $generation kWh\n\n" +
+                    "Consumption : $consumption kWh\n\n" +
+                    "Savings : ₹$savings\n\n" +
+                    "Green Score : $greenScore%\n\n" +
+                    status
+    }
+}
